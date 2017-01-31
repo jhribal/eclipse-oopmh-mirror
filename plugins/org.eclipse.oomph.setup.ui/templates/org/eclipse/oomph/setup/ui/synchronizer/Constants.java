e41b8c32f45dc484c481cc7e12ea187c799948eb
Brian de Alwis <bsd@mt.ca>
2017-01-31 13:16:41 -0500

    [511407] Oomph should adopt USS OAuth authorization
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=511407
    
    OAuth credentials must be set as part of the build.
    
    Signed-off-by: Brian de Alwis <bsd@mt.ca>
    Signed-off-by: Eike Stepper <stepper@esc-net.de>
    Change-Id: I362c567068845975c8019f46d818a615d84d09c3

---
+++ plugins/org.eclipse.oomph.setup.ui/templates/org/eclipse/oomph/setup/ui/synchronizer/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Manumitting Technologies Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Manumitting Technologies Inc. - initial API and implementation
+ */
+package org.eclipse.oomph.setup.ui.synchronizer;
+
+/**
+ * Placeholder constants, filled in at compile-time.
+ */
+public class Constants
+{
+
+  public static final String CLIENT_ID = "@oomph.oauth.clientId@";
+
+  public static final String CLIENT_SECRET = "@oomph.oauth.clientSecret@";
+
+}
